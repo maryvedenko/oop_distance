81cc9c41e2377e9bcd2e7d66caf4976a645ad20b
maryvedenko <xw6g57pzvs@privaterelay.appleid.com>
2025-12-18 01:38:27 +0200

    Create Startup.java

---
+++ first_term/second_lab/part1/Startup.java
@@ -0,0 +1,11 @@
+public class Startup {
+    private int balance;
+
+    public Startup(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return this.balance;
+    }
+}
